1081c836a88720157560d7760997b57738d6386b
lndsilva <lndsilva@hotmail.com>
2023-09-04 11:55:38 -0300

    variaveis e estrutura de decisão

---
+++ js/estrutura_decisao.js
@@ -0,0 +1,15 @@
+//estrutura de decisão
+let a = 10;
+let b = 20;
+//if estrutura básica
+if(a>b){
+    console.log("Entrou na estrutura");    
+}
+//if aninhado
+if(a<b){
+    if(b>a){
+        if(a!=b){
+            console.log("Entrou na estrutura");
+        }
+    }
+}
