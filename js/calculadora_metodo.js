3a78a7ded62b91d9d774cdc39d64e97ea4af4a22
lndsilva <lndsilva@hotmail.com>
2023-09-13 12:00:11 -0300

    metodos e objetos

--- js/calculadora_metodo.js
+++ js/calculadora_metodo.js
@@ -38,7 +38,7 @@ let v1, v2, opcao, op,resp;
 
 v1 = calculadora.num1;
 v2 = calculadora.num2;
-opcao = 1
+opcao = 3
 
 console.log("Calculadora");
 console.log("Insira dos números e escolha a operação desejada.");
@@ -52,15 +52,15 @@ switch (opcao) {
     op = "Soma";
     break;
   case 2:
-    resp = subtracaoValor(v1, v2);
+    resp = subtracao(v1, v2);
     op = "Subtração";
     break;
   case 3:
-    resp = multiplicacaoValor(v1, v2);
+    resp = multiplicacao(v1, v2);
     op = "Multiplicação";
     break;
   case 4:
-    resp = divisaoValor(v1, v2);
+    resp = divisao(v1, v2);
     op = "Divisão";
     break;
 }
